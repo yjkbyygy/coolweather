ac697a1edef8cbc6d6eeda8dfcbfdc011e01377e
yvl <yeslyw@126.com>
2017-06-14 16:23:43 +0800

    create database.

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+
+public class County extends DataSupport {
+    private  int id;
+    private  String countyName;
+    private  int weatherId;
+    private  int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
